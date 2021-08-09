90b79272842d664825463d5d44f19053bef8a33b
Steven Bock <steven@stevenbock.me>
2021-08-09 14:19:59 -0700

    Added useState hook to enable toggling display of website data. Also added conditional CSS based on the useState hook.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BiCalendar, BiTrash } from "react-icons/bi"
+import { BiCalendar } from "react-icons/bi"
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
